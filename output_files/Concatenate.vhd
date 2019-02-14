4570990b025d9a7eb67c87678d73943029521315
rupa99 <47621529+rupa99@users.noreply.github.com>
2019-02-14 16:06:26 -0500

    Update Concatenate.vhd

--- output_files/Concatenate.vhd
+++ output_files/Concatenate.vhd
@@ -2,18 +2,21 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+-- entity declaration for inputs and outputs 
 entity concatenate is port (
-	input_1 : in std_logic_vector(3 downto 0);
-	input_2 : in std_logic_vector(7 downto 4);
-	output_concatenate : out std_logic_vector(7 downto 0)
+	input_1 : in std_logic_vector(3 downto 0); -- declaring 4- bit input vector 
+	input_2 : in std_logic_vector(7 downto 4); -- declaring 4- bit input vector
+	output_concatenate : out std_logic_vector(7 downto 0) -- declaring 8- bit output vector
 );
 
-end concatenate;
+end concatenate; -- ending entity declaration 
 
+--initiating the architecture section 
 architecture Behavioral of concatenate is
 
 begin
 
-	output_concatenate <= input_1 & input_2;
+	output_concatenate <= input_1 & input_2; -- concatenating the 4 bit inputs and assigning the resultant to the 8-bit output vector 
 
 end architecture Behavioral;
+--ending the declaration of the architecture section 

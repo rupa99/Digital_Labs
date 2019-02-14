84700612646d4cc53a532cf3c8bedc9555d93930
rupa99 <47621529+rupa99@users.noreply.github.com>
2019-02-14 00:41:49 -0500

    Update mux_1.vhd
    Mux after making changes

--- output_files/mux_1.vhd
+++ output_files/mux_1.vhd
@@ -2,19 +2,19 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity mux_1 is
-   port (
-          pb        	: in  std_logic;	
-			 DIN2 		: in  std_logic_vector(7 downto 0);	-- Concatenated
-			 DIN1 		: in  std_logic_vector(7 downto 0); -- Sum
-			 DOUT			: out	std_logic_vector(7 downto 0)                                                                                                                                                                                                                                
+entity mux_1 is --- declaring entity 
+   port ( 
+         		 pb        	: in  std_logic;	 ---- recieving input from pb(3)
+			 DIN2 		: in  std_logic_vector(7 downto 0);---Recieving the input from concatenation of the inputs from the switches 
+			 DIN1 		: in  std_logic_vector(7 downto 0); -- Recieving the input from the addition of the inputs after converting it back to an 8- bit logic vector 
+			 DOUT		: out	std_logic_vector(7 downto 0)  ---- Assigning the output from the mux to this output vector                                                                                                                                                                                                                
         );
 end entity mux_1;
 
-architecture syn of mux_1 is
+architecture syn of mux_1 is --- initiating the architecture section 
 
 begin 
 
-DOUT <= (DIN1) WHEN (NOT(pb) = '1')	ELSE (DIN2);
+DOUT <= (DIN1) WHEN (NOT(pb) = '1')	ELSE (DIN2); --- when push button 3 is pushed meaning- its a 0, then select DIN1 which is the addition result, if not select the concatenated input 
 
-end architecture syn;
+end architecture syn; --- end of the architeture section 
